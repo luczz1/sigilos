0d242a217069e1da7809a84aff60d96b3c3b5f78
Luczz <lucaslcsteam@hotmail.com>
2023-07-01 21:04:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sigilos';
+  @ViewChild('originalText', { static: false }) originalText: ElementRef | any;
+
+  public title = 'sigilos';
+  public textWrote = '';
+  textConverted = '';
+
+  constructor() {}
+
+  getFormattedText() {
+    return this.textWrote.replace(/\n/g, '<br>');
+  }
+
+  downloadImage() {
+    const element = this.originalText.nativeElement;
+
+    html2canvas(element, {
+      backgroundColor: null,
+      scale: 2,
+      logging: true,
+      width: element.offsetWidth,
+      height: element.offsetHeight,
+    }).then((canvas) => {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          saveAs(blob, 'sigilos.png');
+        } else {
+          console.error('Erro ao gerar o blob da imagem.');
+        }
+      });
+    });
+  }
 }
